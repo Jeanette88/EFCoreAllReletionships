c5b33ccc7df74ddcccc4b17083d4219645c87f09
Jeanette Sonnerborg <jv_88_@hotmail.com>
2022-08-17 17:01:21 +0200

    Addad - DataContext + ConnectionString

--- EFCoreAllReletionships/Program.cs
+++ EFCoreAllReletionships/Program.cs
@@ -1,11 +1,14 @@
 
+global using EFCoreAllReletionships.Data;
+global using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("Default")));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
