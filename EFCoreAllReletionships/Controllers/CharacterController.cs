4d51c915db7dc23900a5afe6dca205f27cb59706
Jeanette Sonnerborg <jv_88_@hotmail.com>
2022-08-17 17:49:31 +0200

    CharacterController

---
+++ EFCoreAllReletionships/Controllers/CharacterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreAllReletionships.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public CharacterController(DataContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
