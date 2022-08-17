e3e38bf6de308d8481c2087a32407396a34fe70d
Jeanette Sonnerborg <jv_88_@hotmail.com>
2022-08-17 20:08:07 +0200

    CharacterController : [HttpPost("weapon")]  - AddWeapon

--- EFCoreAllReletionships/Controllers/CharacterController.cs
+++ EFCoreAllReletionships/Controllers/CharacterController.cs
@@ -46,5 +46,26 @@ namespace EFCoreAllReletionships.Controllers
             return await Get(newCharacter.UserId);
         }
 
+        [HttpPost("weapon")]
+        public async Task<ActionResult<Character>> AddWeapon(AddWeaponDto request)
+        {
+            var character = await _context.Characters.FindAsync(request.CharacterId);
+            if (character == null)
+            {
+                return NotFound();
+            }
+
+            var newWeapon = new Weapon
+            {
+                Name = request.Name,
+                Damage = request.Damage,
+                Character = character
+            };
+
+            _context.Weapons.Add(newWeapon);
+            await _context.SaveChangesAsync();
+
+            return character;
+        }
     }
 }
