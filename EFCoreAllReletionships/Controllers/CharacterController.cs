57fddc6e0da5aa8a77bda64fa1a064bd20799298
Jeanette Sonnerborg <jv_88_@hotmail.com>
2022-08-17 20:43:11 +0200

    AddCharacterSkillDto Model + [HttpPost("skill")] AddCharacterSkill - Character

--- EFCoreAllReletionships/Controllers/CharacterController.cs
+++ EFCoreAllReletionships/Controllers/CharacterController.cs
@@ -30,9 +30,7 @@ namespace EFCoreAllReletionships.Controllers
         {
             var user = await _context.Users.FindAsync(request.UserId);
             if (user == null)
-            {
                 return NotFound();
-            }
 
             var newCharacter = new Character
             {
@@ -52,9 +50,7 @@ namespace EFCoreAllReletionships.Controllers
         {
             var character = await _context.Characters.FindAsync(request.CharacterId);
             if (character == null)
-            {
                 return NotFound();
-            }
 
             var newWeapon = new Weapon
             {
@@ -64,6 +60,28 @@ namespace EFCoreAllReletionships.Controllers
             };
 
             _context.Weapons.Add(newWeapon);
+            await _context.SaveChangesAsync();
+
+            return character;
+        }
+
+        [HttpPost("skill")]
+        public async Task<ActionResult<Character>> AddCharacterSkill(AddCharacterSkillDto request)
+        {
+
+            var character = await _context.Characters
+                .Where(c => c.Id == request.CharacterId)
+                .Include(c => c.Skills)
+                .FirstOrDefaultAsync();
+            if (character == null)
+                return NotFound();
+
+
+            var skill = await _context.Skills.FindAsync(request.SkillId);
+            if (skill == null)
+                return NotFound();
+
+            _context.Skills.Add(skill);
             await _context.SaveChangesAsync();
 
             return character;
